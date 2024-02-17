a470d5b4cff53b0d8efd9f6a829edbdf406e2a5b
Luchin151090 <lgonzalesu@ulasalle.edu.pe>
2024-02-16 22:53:05 -0500

    updatecolores

--- lib/components/empleado/colores.dart
+++ lib/components/empleado/colores.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
  List<Color> containerColors = [
-    Color.fromARGB(255, 213, 195, 195),
+    Color.fromARGB(255, 54, 35, 110),
     Color.fromARGB(255, 20, 77, 124),
     Color.fromARGB(255, 38, 99, 40),
-    Colors.orange,
+    const Color.fromARGB(255, 47, 32, 10),
     Colors.purple,
     Colors.teal,
     Colors.pink,
